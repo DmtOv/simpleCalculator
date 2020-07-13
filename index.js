943acb32c0647a931dad8699431a589dd6404c12
Dmitry Ovcharenko <Dmitry.OVCHARENKO@raiffeisen.ru>
2020-07-14 02:56:32 +0300

    add calculation

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const init = () => {
-  initClear();
-  initButtonActions();
+    initClear();
+    initButtonsEvents();
 }
 
 const initClear = () => {
-    document.onkeydown = function(evt) {
+    document.onkeydown = (evt) => {
         evt = evt || window.event;
-        if (evt.keyCode == 27) {
-            document.getElementsByClassName('calculator__screen')[0].innerText = '0';
+        if (evt.keyCode === 27) {
+            getScreen().innerText = '0';
             console.log('cleared screen');
         }
     };
 }
 
-const initButtonActions = () => {
+const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+const getScreen = () => {
+    return document.getElementsByClassName('calculator__screen')[0];
 }
 
+const initButtonsEvents = () => {
+    const buttons = document.getElementsByClassName("calculator__button");
+
+    Array.from(buttons).forEach((el) => {
+        const text = el.innerText;
+        const buttonDigit = parseInt(text, 10);
+        if (digits.includes(buttonDigit)) {
+            addDigitListener(el, buttonDigit);
+        } else {
+            addOperationListener(el, text);
+        }
+    });
+}
+
+const addDigitListener = (el, buttonDigit) => {
+    el.addEventListener('click', () => {
+        if ((getScreen().innerText == "0") && (buttonDigit == "0")) {
+            console.log("Can't duplicate zerous ion screen");
+        } else if ((getScreen().innerText == "0") && (buttonDigit !== "0")) {
+            getScreen().innerText = buttonDigit;
+        } else {
+            getScreen().innerText += buttonDigit;
+        }
+    });
+}
+
+const addOperationListener = (el, text) => {
+    el.addEventListener('click', () => {
+        text === '='
+            ? getScreen().innerText = eval(getScreen().innerText)
+            : getScreen().innerText += text;
+    });
+}
